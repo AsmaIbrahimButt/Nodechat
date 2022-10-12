84307a520f461afc32a638d7bb1c3340325ce221
AsmaButt <asmaibrahim657@gmail.com>
2022-10-12 04:05:57 -0700

    node chat

---
+++ nodeServer/index.js
@@ -0,0 +1,17 @@
+// Node server which will handle soclet io connection
+
+
+const io=require('socket.io')(8000)
+
+const users={};
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+        console.log("new user",name)
+        users[socket.id]=name;
+        socket.broadcast.emit('user-joined',name);
+    })
+socket.on('send',message=>{
+    socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+}) ;
+    
+})
